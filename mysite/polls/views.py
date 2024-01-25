8ac83e4c00df2ac8d8c956205b0141d353c39bab
erin <erin@gmail.com>
2024-01-25 23:45:26 +0000

    create form and cut down code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,29 +1,28 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+
 from .models import Choice, Question
 
-# Create your views here.
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """
-        Return the last five published questions.
-        """
+        """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
 
 
-class DetailView(generic.ListView):
+class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
 
-class ResultsView(generic.ListView):
+class ResultsView(generic.DetailView):
     model = Question
-    template_name = "polls/details.html"
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
